29f7f5e9c5dc3f72486e5156b349bd82fbedbe48
Russell Coleman <russellsayshi@gmail.com>
2019-01-26 15:44:25 -0600

    changed dependencies and modified index.js

--- index.js
+++ index.js
@@ -3,21 +3,16 @@ const fs = require('fs');
 const app = express();
 const reids = require("redis"),
 	client = redis.createClient();
-const smartcar = require("smartcar");
+const request = require("request");
 const port = 3000;
 
 client.on("error", function (err) {
 	console.log("Error " + err);
 });
 
+
+
 const secrets = JSON.parse(fs.readFileSync("secrets.txt"));
-const client = new smartcar.AuthClient({
-  clientId: secrets['id'],
-  clientSecret: secrets['sec'],
-  redirectUri: 'YOUR_CALLBACK_URI',
-  scope: ['read_vehicle_info'],
-  testMode: true, // launch the Smartcar auth flow in test mode
-});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
