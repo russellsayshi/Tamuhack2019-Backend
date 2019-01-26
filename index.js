fd89439ba553a010c0cfb209124443656c5bc507
Russell Coleman <russellsayshi@gmail.com>
2019-01-26 15:02:25 -0600

    changes to index.js and removed mongo and installed redis

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
+const reids = require("redis"),
+	client = redis.createClient();
 const port = 3000;
+
+client.on("error", function (err) {
+	console.log("Error " + err);
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/auth_token', function(req, res) {
-	var code = req.query.code;
+	let code = req.query.code;
+	let state_encoded = req.query.state;
+	let state = null;
+	try {
+		state = JSON.parse(state_encoded);
+	} catch(err) {
+		res.status(500).send({error: 'invalid JSON'});
+		return;
+	}
 	
 });
 
